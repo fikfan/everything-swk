b58db2e792a0efdbfb4441c818727051615a0ffc
Fikri <fikfan@gmail.com>
2024-09-03 23:07:21 +0800

    render all business values

--- pages/business/[id].vue
+++ pages/business/[id].vue
@@ -3,7 +3,11 @@
     <div v-if="business">
       <h1 class="text-2xl font-bold mb-4">{{ business.name }}</h1>
       <p>{{ business.description }}</p>
-      <!-- Add more detailed business information here -->
+      <p>{{ business.category }}</p>
+      <p>{{ business.location }}</p>
+      <p>{{ business.hours }}</p>
+      <p>{{ business.status }}</p>
+      <p>{{ business.phone }}</p>
       <Button @click="$router.back()">Back</Button>
     </div>
     <div v-else>Loading...</div>
